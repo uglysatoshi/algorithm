9f0ee1d56a2a669b7e7b7f01d9fbb7094cfc3bd3
satoshi <uglysatoshi@ya.ru>
2024-03-14 22:48:38 +0300

    dz-3-1 allcups

---
+++ all-cups/dz-3-1.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	_, _ = fmt.Scan(&n)
+	arr := make([]int, n)
+	for i := 0; i < len(arr); i++ {
+		_, _ = fmt.Scan(&arr[i])
+	}
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = key
+	}
+
+	for _, v := range arr {
+		fmt.Printf("%d ", v)
+	}
+}
--- leetcode/1.go
+++ leetcode/1.go
@@ -2,18 +2,22 @@ package main
 
 import "fmt"
 
-func twoSum(nums []int, target int) []int {
+func twoSum(nums []int,
+
+	target int) []int {
 	answer := make([]int, 2)
 	for i := 0; i < len(nums); i++ {
-		for j := 0; j < len(nums); j++ {
-			if i != j && nums[i]+nums[j] == target {
+		for j :=
+
+			0; j < len(nums); j++ {
+			if i != j &&
+				nums[i]+nums[j] == target {
 				answer[0] = i
 				answer[1] = j
 				return answer
 			}
 		}
 	}
-
 	return nil
 }
 
